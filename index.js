11050bbf0a6bf0256d995b91075f2b7a4c1fa5a8
Marcos <dev.marcosanjos@gmail.com>
2024-10-12 22:40:53 -0300

    feat: add router to delete task

--- index.js
+++ index.js
@@ -30,4 +30,23 @@ app.post('/tasks', async (req, res) => {
    }
 })
 
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const taskId = req.params.id
+
+        const taskToDelete = await TaskModel.findById(taskId)
+
+        if(!taskToDelete){
+            return res.status(500).send('Essa tarefa não foi encontrada')
+        }
+
+        const deleteTask = await TaskModel.findByIdAndDelete(taskId)
+
+        res.status(200).send(deleteTask)
+        
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
+
 app.listen(8000, () => console.log('Server on port 8000!'))
