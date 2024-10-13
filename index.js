d38d8363efa84eb492bceade396fc215247a9052
Marcos <dev.marcosanjos@gmail.com>
2024-10-12 23:40:35 -0300

    feat: add router file to task

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const TaskRouter = require('./src/routes/taks.routes')
 
 const connectToDatabase = require('./src/database/mongose.database')
-const TaskModel = require('./src/models/task.model')
 
 dotenv.config()
 const app = express()
@@ -10,85 +10,6 @@ app.use(express.json())
 
 connectToDatabase()
 
-app.get('/tasks', async (req, res) => {
-    try {
-        const task = await TaskModel.find({});
-        res.status(200).send(task);
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    try {
-        const taskId = req.params.id
-        const task = await TaskModel.findById(taskId);
-
-        // if(task) return console.log('ok')
-        
-        res.status(200).send(task)
-
-
-    } catch (error) {
-        res.status(404).send("Essa tarefa não foi encontrada");
-    }
-})
-
-app.post('/tasks', async (req, res) => {
-   try {
-     const newTask = new TaskModel(req.body);
-
-     await newTask.save();
-     res.status(201).send(newTask);
-   } catch (error) {
-        res.status(500).send(error.message)
-   }
-})
-
-app.patch('/tasks/:id', async (req, res) => {
-    try {
-        const taskId = req.params.id
-        const taskData = req.body
-        const taskToUpdate = await TaskModel.findById(taskId)
-        
-        const allowedUpdates = ['isCompleted']
-
-        const requestedUpdates = Object.keys(req.body)
-
-        for (update of requestedUpdates) {
-          if (allowedUpdates.includes(update)) {
-            taskToUpdate[update] = taskData[update];
-          } else {
-            return res.status(500).send('Um ou mais campos não são editaveis.')
-          }
-        }
-
-        await taskToUpdate.save(taskToUpdate)
-
-        return res.status(200).send()
-
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
-
-app.delete('/tasks/:id', async (req, res) => {
-    try {
-        const taskId = req.params.id
-
-        const taskToDelete = await TaskModel.findById(taskId)
-
-        if(!taskToDelete){
-            return res.status(404).send('Essa tarefa não foi encontrada')
-        }
-
-        const deleteTask = await TaskModel.findByIdAndDelete(taskId)
-
-        res.status(200).send(deleteTask)
-        
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-})
+app.use("/tasks", TaskRouter);
 
 app.listen(8000, () => console.log('Server on port 8000!'))
